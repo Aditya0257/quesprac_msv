7e34215779ac062ea23ca051485306511d614e15
Aditya0257 <adityasinghthakur.0257@gmail.com>
2024-06-21 18:44:09 +0530

    second commit - updated app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { QuestionsModule } from './modules/questions/questions.module';
+import { AttendeeModule } from './modules/attendees/attendee.module';
+import { AnswerModule } from './modules/answers/answer.module';
+import { SubtopicsModule } from './modules/subtopics/subtopics.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-  imports: [QuestionsModule, PrismaModule],
+  imports: [
+    QuestionsModule,
+    PrismaModule,
+    AttendeeModule,
+    AnswerModule,
+    SubtopicsModule,
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
